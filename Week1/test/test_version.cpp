b44433d31d545bccb3f453d9d830355aa89848ce
Anna <avederojo@gmail.com>
2023-01-30 11:35:22 +0300

    Moved googletest up

--- Week1/test/test_version.cpp
+++ Week1/test/test_version.cpp
@@ -1,5 +1,5 @@
 
-#include "lib.h"
+#include "../src/lib.h"
 #include <gtest/gtest.h>
 
 // Demonstrate some basic assertions.
@@ -7,7 +7,7 @@ TEST(HelloTest, BasicAssertions) {
   // Expect two strings not to be equal.
   EXPECT_STRNE("hello", "world");
   // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+  EXPECT_TRUE(version() > 0);
 }
 
 //BOOST_AUTO_TEST_SUITE(test_version)
