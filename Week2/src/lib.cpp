c7c470dec6a0777351807615f21b7b2762722e66
Anna <avederojo@gmail.com>
2023-01-31 15:50:39 +0300

    Task for Week 2

---
+++ Week2/src/lib.cpp
@@ -0,0 +1,20 @@
+#include "lib.h"
+
+std::vector<std::string> split(const std::string &str, char d)
+{
+    std::vector<std::string> r;
+
+    std::string::size_type start = 0;
+    std::string::size_type stop = str.find_first_of(d);
+    while(stop != std::string::npos)
+    {
+        r.push_back(str.substr(start, stop - start));
+
+        start = stop + 1;
+        stop = str.find_first_of(d, start);
+    }
+
+    r.push_back(str.substr(start));
+
+    return r;
+}
---
+++ Week2/src/lib.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <vector>
+#include <string>
+
+std::vector<std::string> split(const std::string &str, char d);
